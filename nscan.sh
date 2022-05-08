f28a88e79e39547402ae2b1995e0f71d57782a1b
Indranil Roy <81170328+indranilroy99@users.noreply.github.com>
2022-05-08 20:50:41 +0530

    Add files via upload

---
+++ nscan.sh
@@ -0,0 +1,14 @@
+#!/bin/bash                                              
+                                              
+                                              
+                                               figlet  NSCAN
+                        		 echo Automate  the network scanning 
+                       
+
+echo  "##############################################################################################################"
+
+echo -n "[+] Please enter the Target IP or doamin address >>>"
+read $target
+
+
+
