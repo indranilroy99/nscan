f978b22b84d947110789182db8290bd2d86e6370
Indranil Roy <81170328+indranilroy99@users.noreply.github.com>
2022-05-16 10:46:09 +0530

    Update nscan.sh

--- nscan.sh
+++ nscan.sh
@@ -41,65 +41,66 @@ read choice
 
 case $choice in
   
-  1) clear ;echo  -e "\e[1;37m [+] Performing host scan for $IP  \e[0m" 
+  1) clear ;echo  -e "\e[1;43m [+] Performing host scan for $IP  \e[0m" 
       nmap   $IP;;
   
-  2) clear ;echo  -e "\e[1;37m [+] Performing TCP SYN scan for $IP  \e[0m" 
+  2) clear ;echo  -e "\e[1;43m [+] Performing TCP SYN scan for $IP  \e[0m" 
       nmap  -sS -T4 $IP;;
       
-  3) clear ;echo  -e "\e[1;37m [+] Performing Connect scan for $IP  \e[0m" 
+  3) clear ;echo  -e "\e[1;43m [+] Performing Connect scan for $IP  \e[0m" 
       nmap   -sT -T4 $IP;;   
 
-  4) clear ;echo  -e "\e[1;37m [+] Performing ACK scan for $IP  \e[0m"   
+  4) clear ;echo  -e "\e[1;43m [+] Performing ACK scan for $IP  \e[0m"   
       nmap   -sA  -T4 $IP;;
 
-  5)  clear ;echo  -e "\e[1;37m [+] Performing Window scan for $IP  \e[0m"   
+  5)  clear ;echo  -e "\e[1;43m [+] Performing Window scan for $IP  \e[0m"   
       nmap   -sW -T4 $IP;; 
   
-  6)  clear ;echo  -e "\e[1;37m [+] Performing Maimon scan for $IP  \e[0m"     
+  6)  clear ;echo  -e "\e[1;43m [+] Performing Maimon scan for $IP  \e[0m"     
       nmap   -sM -T4 $IP;; 
-  7) clear ;echo  -e "\e[1;37m [+] Performing UDP scan for $IP  \e[0m"   
+  7) clear ;echo  -e "\e[1;43m [+] Performing UDP scan for $IP  \e[0m"   
       nmap   -sU -T4 $IP;;
   
- 8) clear ;echo  -e "\e[1;37m [+] Performing TCP NULL scan for $IP  \e[0m"   
+ 8) clear ;echo  -e "\e[1;43m [+] Performing TCP NULL scan for $IP  \e[0m"   
       nmap   -sN -T4 $IP;;
 
- 9) clear ;echo  -e "\e[1;37m [+] Performing FIN  scan for $IP  \e[0m"      
+ 9) clear ;echo  -e "\e[1;43m [+] Performing FIN  scan for $IP  \e[0m"      
     nmap   -sF -T4 $IP;;
   
-10) clear ;echo  -e "\e[1;37m [+] Performing Xmas scan for $IP  \e[0m"   
+10) clear ;echo  -e "\e[1;43m [+] Performing Xmas scan for $IP  \e[0m"   
     nmap   -sX -T4 $IP;;
  
-11) clear ;echo  -e "\e[1;37m [+] Performing COOKIE-ECHO  scan for $IP  \e[0m"    
+11) clear ;echo  -e "\e[1;43m [+] Performing COOKIE-ECHO  scan for $IP  \e[0m"    
     nmap   -sZ -T4 $IP;;   
 
-12)clear ;echo  -e "\e[1;37m [+] Performing IP-PROTOCOL scan for $IP  \e[0m"   
+12)clear ;echo  -e "\e[1;43m [+] Performing IP-PROTOCOL scan for $IP  \e[0m"   
     nmap   -sO -T4 $IP;;
 
-13) clear ;echo  -e "\e[1;37m [+] Performing SCRIPT INIT scan for $IP  \e[0m"     
+13) clear ;echo  -e "\e[1;43m [+] Performing SCRIPT INIT scan for $IP  \e[0m"     
     nmap   -sY -T4 $IP;;  
        
 14) clear 
    echo  -en  "\e[1;93m [+] Enter the subnet>>>  \e[0m" 
     read subnet
-      echo  -e "\e[1;37m [+] Performing Netbios scan  for $subnet  \e[0m" 
+      echo  -e "\e[1;43m [+] Performing Netbios scan  for $subnet  \e[0m" 
       nmap -sV -v -p 139,445 $subnet;; 
       
-15) clear ;echo  -e "\e[1;37m [+] Finding Netbios name for $IP  \e[0m" 
+15) clear ;echo  -e "\e[1;43m [+] Finding Netbios name for $IP  \e[0m" 
       nmap -sU — script nbstat.nse -p 137  $IP;;
       
 
-16) clear ;echo  -e "\e[1;37m [+] Performig Ping  scan for $IP  \e[0m" 
+16) clear ;echo  -e "\e[1;43m [+] Performig Ping  scan for $IP  \e[0m" 
       nmap -sP  $IP;;
       
 17)  clear 
       echo  -en  "\e[1;93m [+] Enter the Zombie Hostname>>>  \e[0m" 
        read zombie
-       echo  -e "\e[1;37m [+] Performing Idle Scan for $IP  \e[0m" 
+       echo  -e "\e[1;43m [+] Performing Idle Scan for $IP  \e[0m" 
        nmap  -Pn -p- -sI $zombie $IP;;
  
   *) echo  -e "\e[1;41m   invalid option !    \e[0m" ;;
      
 esac
-echo  -en  "\e[1;36m   QUITTING....   \e[0m" 
+echo  -en  "\e[1;36m QUITTING....   \e[0m" 
 sleep 2
+
